9bea4ee433a1f31997020ac2ac74b6354388bb18
Brooke Perry and Kristin Brewer-Lowe <kristin@>
2018-10-23 10:46:04 -0700

    rewrite js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,52 @@
-function changeVowelsToDash (array) {
-  var vowelsReplacedWithDashes = [];
-  array.forEach(function(element){
-    if (element === "a" || element === "e" || element === "i" || element === "o" || element === "u" ) {
-      vowelsReplacedWithDashes.push("-");
-    } else {
-      vowelsReplacedWithDashes.push(element)  ;
-    }
+// function changeVowelsToDash (array) {
+//   var vowelsReplacedWithDashes = [];
+//   array.forEach(function(element){
+//     if (element === "a" || element === "e" || element === "i" || element === "o" || element === "u" ) {
+//       vowelsReplacedWithDashes.push("-");
+//     } else {
+//       vowelsReplacedWithDashes.push(element)  ;
+//     }
+//   });
+//   return vowelsReplacedWithDashes;
+// }
+//
+// $(document).ready(function() {
+//   $("#form").submit(function(event) {
+//     event.preventDefault();
+//
+//     var sentence = ($("input#sentence").val()).split("");
+//     console.log(sentence);
+//
+//     changeVowelsToDash(sentence);
+//
+//
+//   });
+// });
+function changeVowelsToDash(array) {
+  var vowelsReplacedWithDashes = []
+  array.forEach(function(letters) {
+    if (letters === "a" || letters === "e" || letters === "i" || letters === "o" || letters === "u") {
+    vowelsReplacedWithDashes.push("-");
+  } else {
+    vowelsReplacedWithDashes.push(letters);
+  }
   });
   return vowelsReplacedWithDashes;
 }
 
+
+
 $(document).ready(function() {
   $("#form").submit(function(event) {
-    event.preventDefault();
-
-    var sentence = ($("input#sentence").val()).split("");
-    console.log(sentence);
-
-    changeVowelsToDash(sentence);
+  event.preventDefault();
 
 
-  });
+  var sentence = ($("input#sentence").val()).split("");
+var newSentence = changeVowelsToDash(sentence);
+
+  console.log(newSentence);
+
+});
+
+
 });
